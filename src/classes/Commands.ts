30179750750d6514267120045f59d52e17953b29
Jasper v.H <jasper.van.hienen@gmail.com>
2018-08-06 18:40:12 +0200

    Added better descriptions and fixed a bug where the bot would crash when there would be no users

--- src/classes/Commands.ts
+++ src/classes/Commands.ts
@@ -35,7 +35,7 @@ export class Commands extends Bot {
 					let embed = new Discord.RichEmbed()
 					.setTitle("Weeaboo Detector (weeabot) version " + this.package.version)
 					.setColor(this.config.color)
-					.addField("Commands: ", "- !weeabot info: lists info of this bot.")
+					.addField("Commands: ", `Use !weeabot commands, to list all commands.`)
 					.addField("Your server: ", msg.guild.name)
 					.addField("Server ID: ", msg.guild.id)
 					.addField("Bugs: ", `Found a bug? Report it [here](https://github.com/jjasspper/Weeaboo-Detector/issues)`)
@@ -45,6 +45,25 @@ export class Commands extends Bot {
 					break;
 				case firstParam === "serverid":
 					console.log(msg.guild.id);
+					break;
+				case firstParam === "commands":
+					let commandEmbed = new Discord.RichEmbed()
+					.setTitle("All commands:")
+					.setColor(this.config.color)
+					.addField("Usage", `
+						All commands have to be prefixed by: !weeabot. 
+						In a command description you can find these "{}" tags. These tags indicate a parameter. 
+						When typing a command you should type the parameter without the {}, as these are only an indication of a parameter.
+					`)
+					.addField("info", `Lists info of this bot`)
+					.addField("whitelist {@user}", `Exludes a server member of being tracked by this bot. All previous stats will remain saved.`)
+					.addField("update mutelevel {number}", `Updates the maximum level a server member can have before being muted.`)
+					.addField("update kicklevel {number}", `Updates the maximum level a server member can have before being kicked.`)
+					.addField("update banlevel {number}", `Updates the maximum level a server member can have before being banned.`)
+					.addField("weeablevel {@user}", `Gets the weeablevel of the mentioned server member. Only the first mention will be used.`)
+					.setFooter("© JVH 2018")
+					.setThumbnail(this.client.user.avatarURL);
+					Message.send(commandEmbed, msg.channel);
 					break;
 				case firstParam === "update":
 					let level: number = parseInt(thirdParam);
@@ -95,10 +114,15 @@ export class Commands extends Bot {
 					const watchlist = new Watchlist();
 					let userID = msg.mentions.users.values().next().value.id;
 
-					watchlist.getLevel(msg.guild.id, userID).then((result:string) => {
+					watchlist.getLevel(msg.guild.id, userID).then((result: string) => {
 						let response = JSON.parse(result);
 						let data = response.data[0];
-						Message.send(`The weeab-level of ${data.user} is ${data.level}`, msg.channel);
+						console.log(data);
+						if (data) {
+							Message.send(`The weeab-level of ${data.user} is ${data.level}`, msg.channel);
+						} else {
+							Message.send(`The mentioned user has no tracked stats.`, msg.channel);
+						}
 					}, (err) => {
 						console.log(err);
 					});
